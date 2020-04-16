657b9c8b8f49650ce636029300a23395ed340753
Kosio <kosiokarchev@gmail.com>
2020-04-16 07:32:12 +0300

    rgb colormaps

--- gptrip/colors/__init__.py
+++ gptrip/colors/__init__.py
@@ -1,14 +1,24 @@
 import os
 from glob import glob
+from itertools import permutations
 
 import numpy as np
 from matplotlib import cm, colors
 
 
+def register(name, clrs):
+    cm.register_cmap(name, colors.LinearSegmentedColormap.from_list(name, clrs))
+    cm.register_cmap(name+'_r', colors.LinearSegmentedColormap.from_list(name+'_r', clrs[::-1]))
+
+
 for fname in glob(os.path.join(os.path.dirname(__file__), '*.cmap')):
-    cm.register_cmap(cmap=colors.ListedColormap(
-        colors=np.loadtxt(fname, dtype=int)/255.,
-        name=os.path.basename(fname)[:-5]
-    ))
+    register(os.path.basename(fname)[:-5], np.loadtxt(fname, dtype=int)/255.)
+    # cm.register_cmap(cmap=colors.ListedColormap(
+    #     colors=np.loadtxt(fname, dtype=int)/255.,
+    #     name=os.path.basename(fname)[:-5]
+    # ))
+
+for i in permutations(range(3)):
+    register(''.join(np.array(('r', 'g', 'b'))[i,]), np.eye(3)[i,])
 
 __all__ = []
